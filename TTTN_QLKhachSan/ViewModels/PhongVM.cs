530d51d285415f4f61da7fc51e12c3237bcaa5d8
LENOVO <LENOVO@DESKTOP-SB7ARCO>
2024-04-16 13:02:46 +0700

    Add project files.

---
+++ TTTN_QLKhachSan/ViewModels/PhongVM.cs
@@ -0,0 +1,17 @@
+﻿using TTTN_QLKhachSan.Models;
+
+namespace TTTN_QLKhachSan.ViewModels
+{
+    public class PhongVM
+    {
+        public int Id { get; set; }
+        public string TenLoaiPhong { get; set; }
+
+        public decimal GiaPhong { get; set; }
+
+        public int SoGiuong { get; set; }
+        public string MoTa { get; set; }
+
+        public virtual Loaiphong LoaiPhongIdIdNavigation { get; set; }
+    }
+}
